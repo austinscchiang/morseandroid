bb83507458687fb0e94ffee3d6bc16e8618a8ed5
Austin Chiang <ausitnscchiang@gmail.com>
2013-04-26 18:53:15 -0400

    second commit, getting used to android layout

--- gen/com/ac/austin/R.java
+++ gen/com/ac/austin/R.java
@@ -28,16 +28,20 @@ public final class R {
         public static final int edit_message=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_display_message=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050003;
         public static final int edit_message=0x7f050002;
+        public static final int hello_world=0x7f050005;
+        public static final int title_activity_display_message=0x7f050004;
     }
     public static final class style {
         /** 
--- src/com/ac/austin/MainActivity.java
+++ src/com/ac/austin/MainActivity.java
@@ -3,15 +3,25 @@ package com.ac.austin;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	public void sendMessage(View view){
+		Intent intent=new Intent (this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+		//do shit
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
