9c2d6e673c40ad82a05da451750dd59f5899a96a
Austin Chiang <ausitnscchiang@gmail.com>
2013-04-29 23:32:01 -0400

    unfinished implementation of morse decoding

--- src/com/ac/austin/MainActivity.java
+++ src/com/ac/austin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ac.austin;
 
+import java.util.*;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -14,11 +15,59 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	public void switchChar(String symbol, String message){
+		HashMap hm =new HashMap();
+		hm.put(".-", "A");
+		hm.put("-...", "B");
+		hm.put("-.-.", "C");
+		hm.put("-..", "D");
+		hm.put(".", "E");
+		hm.put("..-.", "F");
+		hm.put("--.", "G");
+		hm.put("....", "H");
+		hm.put("..", "I");
+		hm.put(".---", "J");
+		hm.put("-.-", "K");
+		hm.put(".-..", "L");
+		hm.put("--", "M");
+		hm.put("-.", "N");
+		hm.put("---", "O");
+		hm.put(".--.", "P");
+		hm.put("--.-", "Q");
+		hm.put(".-.", "R");
+		hm.put("...", "S");
+		hm.put("-", "T");
+		hm.put("..-", "U");
+		hm.put("...-", "V");
+		hm.put(".--", "W");
+		hm.put("-..-", "X");
+		hm.put("-.--", "Y");
+		hm.put("--..", "Z");
+		Iterator iter = hm.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry mEntry = (Map.Entry) iter.next();
+			if(mEntry.getKey()==symbol){
+				message+=mEntry.getValue();
+				//System.out.println(mEntry.getValue());
+			}
+		}
+		
+	}
+	public String decodeMorse(String message){
+		//System.out.println("Hello");
+		String[]words=message.split(" ");
+		String decoded=null;
+		for(String word:words){
+			switchChar(word,decoded);
+		}
+		return decoded;
+				
+	}
 	public void sendMessage(View view){
 		Intent intent=new Intent (this, DisplayMessageActivity.class);
 		EditText editText = (EditText) findViewById(R.id.edit_message);
 		String message = editText.getText().toString();
-		intent.putExtra(EXTRA_MESSAGE, message);
+		intent.putExtra(EXTRA_MESSAGE, decodeMorse(message));
 		startActivity(intent);
 		//do shit
 	}
