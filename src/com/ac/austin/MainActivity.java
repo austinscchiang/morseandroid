0e67d6811c815b0ec52274522461443c696bd920
austinscchiang <austinscchiang@gmail.com>
2013-06-04 17:09:14 -0400

    finished hashmap implementation

--- src/com/ac/austin/MainActivity.java
+++ src/com/ac/austin/MainActivity.java
@@ -18,8 +18,8 @@ import android.widget.ToggleButton;
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 	public static String MODE = "ENCODE";
-	public static HashMap <Character, String> charToMorseMap = new HashMap<Character, String>();
-	public static HashMap <String, Character> morseToCharMap = new HashMap<String, Character>();
+	public static HashMap <Character, String> charToMorse = new HashMap<Character, String>();
+	public static HashMap <String, Character> morseToChar = new HashMap<String, Character>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,57 +49,41 @@ public class MainActivity extends Activity {
 	
 	
 	
-	private void setHash(HashMap hm, String fileName){
+	private void setHash(HashMap mToC,HashMap cToM){
 		int asciiCount;
 		try{
+			String line;
 			AssetManager am = getAssets();
 			InputStream inputStream = am.open("morsesource.txt");
 			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 			BufferedReader fileReader = new BufferedReader(inputStreamReader);
-			for(asciiCount=0;asciiCount<96; asciiCount++){
-				if(asciiRange(asciiCount)){
-					hm.put((char)asciiCount, fileReader.readLine());
-				}
-			}
-		}
-		catch(Exception e){
-			System.out.println("could not open");
-		}
-	}
-	
-	private char switchDecodeChar(String symbol){
-		String line;
-		int asciiCount;
-		try{
-			AssetManager am = getAssets();
-			InputStream inputStream = am.open("morsesource.txt");
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-			BufferedReader fileReader = new BufferedReader(inputStreamReader);
-			for(asciiCount=0;asciiCount<96; asciiCount++){
+			for(asciiCount=0;asciiCount<130; asciiCount++){
 				if(asciiRange(asciiCount)){
 					line=fileReader.readLine();
-					if(symbol.equals(line)){
-						fileReader.close();
-						return(char)asciiCount;
-					}
-					
-				}else if(symbol.equals("/")||symbol.equals("\n")){
-					return ' ';
+					cToM.put((char)asciiCount, line);
+					mToC.put(line, (char)asciiCount);
 				}
 			}
 		}
 		catch(Exception e){
 			System.out.println("could not open");
-			return '<';
 		}
-		return '?';
 	}
 	
-	private String decodeMorse(String message){
+	private char switchDecodeChar(String symbol, HashMap<String, Character> hm){
+		try{
+			return hm.get(symbol);
+		}
+		catch(Exception e){
+			return '?';
+		}
+	}
+	
+	private String decodeMorse(String message, HashMap morseToChar){
 		String[]words=message.split(" ");
 		String decoded="";
 		for(String word:words){
-			decoded+=switchDecodeChar(word);
+			decoded+=switchDecodeChar(word, morseToChar);
 		}
 		return decoded;				
 	}
@@ -108,34 +92,17 @@ public class MainActivity extends Activity {
 	
 	
 	
-	private String switchEncodeChar(char symbol){
-		String line;
-		int asciiCount=65;
+	private String switchEncodeChar(char symbol, HashMap<Character, String> hm){
 		try{
-			AssetManager am = getAssets();
-			InputStream inputStream = am.open("morsesource.txt");
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-			BufferedReader fileReader = new BufferedReader(inputStreamReader);
-			for(asciiCount=0;asciiCount<123; asciiCount++){
-				if(asciiRange(asciiCount)){
-					line=fileReader.readLine();
-					if(symbol==(char)(asciiCount)){
-										fileReader.close();
-						return line+=" ";
-					}
-					
-				}
-			}
+			return hm.get(symbol);
 		}
 		catch(Exception e){
-			System.out.println("could not open");
-			return "B";
+			return "?";
 		}
-		return "C";
 	}
 	
 	
-	private String encodeMorse(String message){
+	private String encodeMorse(String message, HashMap charToMorse){
 		String output="";
 		String[]paragraphs=message.split("\n");
 		for (String paragraph:paragraphs){
@@ -143,7 +110,8 @@ public class MainActivity extends Activity {
 			for(String word:words){
 				char[] characters=word.toCharArray();
 				for (char character:characters){
-					output+=switchEncodeChar(character);
+					output+=switchEncodeChar(character, charToMorse);
+					output+=" ";
 				}
 				output+="/ ";
 			}
@@ -190,15 +158,16 @@ public class MainActivity extends Activity {
 	}
 	
 	public void sendMessage(View view){
+		setHash(morseToChar, charToMorse);
 		EditText editText = (EditText) findViewById(R.id.edit_message);
 		String message = editText.getText().toString();
 		System.out.println(message);
 		//startActivity(intent);
 		EditText output = (EditText)findViewById(R.id.outputText);
 		if(MODE.equals("ENCODE")){
-			output.setText(encodeMorse(message));
+			output.setText(encodeMorse(message, charToMorse));
 		}else if (MODE.equals("DECODE")){
-			output.setText(decodeMorse(message));
+			output.setText(decodeMorse(message, morseToChar));
 		}
 		output.setVisibility(View.VISIBLE);
 	}
